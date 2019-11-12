6ffcf7de3eb4797f44ce42d9c77410cab2be4d94
RA <ra@iMc27-20vi5.local>
2019-11-12 11:22:15 +0100

    Current status

--- test7-Game_of_life_Change_status.py
+++ test7-Game_of_life_Change_status.py
@@ -3,13 +3,11 @@
 w = int(input("Enter wide : "))
 h = int(input("Enter height : "))
 
-
 print("Please enter Field of Life. Empty cell is '.'. Cell with live is 'X'.")
 field = []
 
 for i in range(h):
     field.append(list(input().ljust(w,'.')[0:w]))
 
-
-
 print(field)
+
